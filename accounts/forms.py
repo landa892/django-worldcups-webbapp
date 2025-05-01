4db25028e9e3fe1b805bfab1fdd60ec98c484472
Nacho Landa <nacholanda08@gmail.com>
2025-05-01 18:31:34 -0300

    Añadidos first_name y last_name al perfil de usuario, y actualizados templates de perfil

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,7 +6,7 @@ class UserRegisterForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
-        fields = ['username','email','password']
+        fields = ['username', 'email', 'password', 'first_name', 'last_name']
 
 class ProfileForm(forms.ModelForm):
     class Meta:
